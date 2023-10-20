eb244b847a655e665ad06ea7d805ed3990b64fc4
Felipe Sartori <sartorifelipe@icloud.com>
2023-10-19 21:27:02 -0300

    Changed CurrencyDataNotFoundException message

--- src/main/java/com/felipe/sartori/WEXinterview/exceptions/CurrencyDataNotFoundException.java
+++ src/main/java/com/felipe/sartori/WEXinterview/exceptions/CurrencyDataNotFoundException.java
@@ -4,6 +4,6 @@ public class CurrencyDataNotFoundException extends Exception {
     private static final long serialVersionUID = 4928599035264976611L;
 
     public CurrencyDataNotFoundException() {
-        super("Error getting currency information");
+        super("The purchase cannot be converted to the target currency");
     }
 }
